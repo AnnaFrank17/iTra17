78c7e36a10d1cad59f6d91a0684e0aaa04130683
Ann <annanova913@gmail.com>
2017-11-30 23:24:34 +0300

    authentification
    вроде работает

---
+++ Auth_test/Auth_test/Startup.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Auth_test.Data;
+using Auth_test.Models;
+using Auth_test.Services;
+
+namespace Auth_test
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication()
+
+            .AddVKontakte(option =>
+            {
+                option.ClientId = Configuration["Authentication:Vkontakte:ClientId"];
+                option.ClientSecret = Configuration["Authentication:Vkontakte:ClientSecret"];
+            })
+
+            .AddFacebook(option =>
+            {
+                option.AppId = Configuration["Authentication:Facebook:AppId"];
+                option.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            })
+
+            .AddTwitter(option =>
+            {
+                option.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+                option.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
